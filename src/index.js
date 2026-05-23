aeaa3f9aed772e8869c46d4abafbbce28cba50a4
Mazisam <maziyarnikeqbal@gmail.com>
2026-05-23 18:02:14 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ const TARGET = "https://pnl.zagrosmaal.ir:8443";
 
 async function handler(request) {
   const url = new URL(request.url);
-  const targetUrl = TARGET + url.pathname + url.search;
 
-  const headers = new Headers(request.headers);
-  headers.set("host", "pnl.zagrosmaal.ir");
+  const response = await fetch(
+    TARGET + url.pathname + url.search,
+    {
+      method: request.method,
+      headers: request.headers,
+      body: request.body
+    }
+  );
 
-  return fetch(targetUrl, {
-    method: request.method,
-    headers,
-    body: request.body,
-    redirect: "manual"
-  });
+  return response;
 }
 
-addEventListener("fetch", (event) => {
+addEventListener("fetch", event => {
   event.respondWith(handler(event.request));
 });
